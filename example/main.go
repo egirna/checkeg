eefdee7b660295d62f8d23ebd0a0827f0228891b
reem-masoud <65132189+reem-masoud@users.noreply.github.com>
2020-06-25 15:52:10 +0200

    Update main.go

--- example/main.go
+++ example/main.go
@@ -1,7 +1,8 @@
 package main
 import (
 	"fmt"
-
+	"time"
+	
 	"github.com/egirna/checkeg"
 )
 
@@ -9,5 +10,6 @@ func main() {
 	
 	fmt.Println(checkeg.CheckUrL("https://golangbot.com/go-packages/", []int{200, 300}))
 	//fmt.Println(checkeg.CheckUrL("https://www.yahoo.com/games", []int{400, 500}))
+	fmt.Println(checkeg.TCPcheck("golang.org", "80", 6*time.Second))
 	
 }
